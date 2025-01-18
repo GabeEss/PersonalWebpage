031abaa41f4fc0a2ba8f60432187739903106184
GabeEss <113389026+GabeEss@users.noreply.github.com>
2025-01-17 21:48:20 -0500

    Add header component.

---
+++ src/components/header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    
+  </script>
+  
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  
+  <style>
+    
+  </style>
