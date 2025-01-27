71610707485426272a204c35732eca1e7ac8c230
Gabe S <gabesliwowicz@gmail.com>
2025-01-27 15:03:28 -0500

    Header styling and new fonts added.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { sveltePreprocess } from 'svelte-preprocess'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
