71dbef7fcc198af4d2c0e74756ae4a03c42a62af
GabeEss <113389026+GabeEss@users.noreply.github.com>
2025-02-01 00:42:46 -0500

    Alter grid and variables

--- src/lib/sections/about.svelte
+++ src/lib/sections/about.svelte
@@ -3,20 +3,13 @@
 </script>
 
 <section>
-    <div class="about-container">
-        <p>Hello, there.</p>
-        <p>My name is</p>
-        <h1>Gabriel Sliwowicz</h1>
-        <p>I'm a Frontend Developer from Toronto with a passion for building applications.</p>
-    </div>
+    <p class="greeting-name">My name is</p>
+    <h1>Gabriel Sliwowicz</h1>
     <img src="/src/assets/me_pic.png" alt="Gabe Ess profile">
+    <p class="greeting-info">I'm a Frontend Developer from Toronto with a passion for building applications.</p>
 </section>
 
 <style>
-    /* h1, p {
-        margin: 0;
-        padding: 0;
-    } */
     section{
         background-color: var(--color-bg-dark);
         width: 100%;
@@ -24,18 +17,53 @@
         grid-template-columns: repeat(8, 1fr);
         grid-template-rows: repeat(8, 1fr);
         overflow-x: hidden;
+        height: 100vh;
     }
 
     .about-container {
         grid-column: 2/5;
-        grid-row: 3/9;
+        grid-row: 4/9;
     }
+
+    .greeting-name {
+        display: flex;
+        align-items: center;
+        gap: var(--spacing-m);
+        font-size: var(--font-size-h3);
+        color: var(--color-primary);
+        grid-row: 2/9;
+        grid-column: 3/9;
+    }
+
+    .greeting-one {
+        
+    }
+
+    .greeting-two {
+        font-size: var(--font-size-h3);
+        color: var(--color-primary);
+    }
+
+    .greeting-info {
+        grid-column: 2/5;
+        grid-row: 9/9;
+    }
+
+    h1 {
+		display: inline-block;
+		font-size: var(--font-size-h2);
+		color: var(--color-secondary);
+		font-weight: bold;
+		padding: var(--spacing-s) 0;
+        grid-row: 5/9;
+        grid-column: 6/9;
+	}
 
     img {
         position: relative;
 		width: 45vh;
-		grid-column: 5 / 9;
-		grid-row: 2/9;
+		grid-column: 4 / 6;
+		grid-row: 4/9;
 		justify-content: end;
     }
 
